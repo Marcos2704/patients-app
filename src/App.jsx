1c14f7873ab3aaaf26c9dd9d889c87ec9889d042
Marcos2704 <nahuelmarcos2001@gmail.com>
2022-07-22 20:37:50 -0300

    creating components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "./components/Form"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Form/>
+      <h1>HOLA MUNDO</h1>
+    </div>
+  )
+}
+
+export default App
