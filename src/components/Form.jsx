3bb4ab736de5419cb072444223ba344c6c4bb767
Marcos2704 <nahuelmarcos2001@gmail.com>
2022-07-23 00:27:32 -0300

    creating states in the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Form = () => {
+  const [nombre, setNombre] = useState("")
+  const [email, setEmail] = useState("")
+  const [sintomas, setSintomas] = useState("")
+  const [fechaAlta, setFechaAlta] = useState("")
+
+  const handleSubmit = (e) =>{
+    e.prventDefault()
+    console.log("Formulario enviado...")
+  }
+
   return (
     
       <div className='md:w-1/2 lg:w-2/5 md:mx-40'>
         <h2 className="text-3xl font-black">Formulario</h2>
         <span className=' my-3 text-2xl font-black text-orange-900'>Añade pacientes</span>
 
-        <form className='my-5 mb-10 bg-white shadow-lg rounded-lg p-5'>
+        <form onSubmit={handleSubmit} className='my-5 mb-10 bg-white shadow-lg rounded-lg p-5'>
           <div className='mb-5'>
             <label htmlFor='paciente' className='block font-bold text-slate-800 uppercase'>Nombre de paciente</label>
             <input
                id='paciente'
                className=' w-full p-2 mt-2 border-2 placeholder-slate-400 uppercase' 
                placeholder='Nombre'
-               type="text" />
+               type="text" 
+               value={nombre}
+               onChange={(e)=>setNombre(e.target.value)} />
           </div>
           <div className='mb-5'>
             <label htmlFor='emial' className='block font-bold text-slate-800 uppercase'>email</label>
@@ -22,7 +35,9 @@ const Form = () => {
                id='email'
                className=' w-full p-2 mt-2 border-2 placeholder-slate-400 uppercase' 
                placeholder='Email '
-               type="email" />
+               type="email" 
+               value={email}
+               onChange={(e)=>setEmail(e.target.value)}/>
           </div>
           <div className='mb-5'>
             <label htmlFor='sintomas' className='block font-bold text-slate-800 uppercase'>sintomas</label>
@@ -30,7 +45,8 @@ const Form = () => {
             id="sintomas" 
             placeholder='Escriba su sintoma'
             className='w-full border-2 rounder-lg'
-
+            value={sintomas}
+            onChange={(e)=>setSintomas(e.target.value)}
             />
           </div>
           <div className='mb-5'>
@@ -38,7 +54,9 @@ const Form = () => {
             <input
                id='alta'
                className=' w-full p-2 mt-2 border-2 placeholder-slate-400 uppercase' 
-               type="date" />
+               type="date"
+               value={fechaAlta}
+               onChange={(e)=>setFechaAlta(e.target.value)} />
           </div>
           <input
             className=' rounded-lg uppercase bg-orange-700 hover:bg-orange-900 cursor-pointer transition-colors w-full p-2 text-white font-black'
